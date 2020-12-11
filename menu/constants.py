ff197c11ef52d828cd11beea5a399b3ccfb6a877
WillRazorFace <willrazorface90s@gmail.com>
2020-12-11 12:03:04 -0300

    Menu constants

---
+++ menu/constants.py
@@ -0,0 +1,34 @@
+from platform import system
+
+OPTIONS_FILE = 'instamax_config'
+
+if system() == 'Windows':
+    CLEAR_CONSOLE_COMMAND = 'cls'
+elif system() == 'Linux':
+    CLEAR_CONSOLE_COMMAND = 'clear'
+
+BANNER = '''
+    ____                   __             __  ___                
+   /  _/   ____    _____  / /_  ____ _   /  |/  /  ____ _   _  __
+   / /    / __ \  / ___/ / __/ / __ `/  / /|_/ /  / __ `/  | |/_/
+ _/ /    / / / / (__  ) / /_  / /_/ /  / /  / /  / /_/ /  _>  <  
+/___/   /_/ /_/ /____/  \__/  \__,_/  /_/  /_/   \__,_/  /_/|_|  
+'''
+
+DRIVER_MENU = '''
+[1] - Chrome
+[2] - Firefox
+[3] - Safari
+'''
+
+MAIN_MENU = '''
+[0] - Reconfigure credentials and driver
+[1] - Follow suggested
+[2] - Like posts from hashtag
+[3] - Get followers from an account
+[4] - Search follower in an account
+[5] - Get following users from an account
+[6] - Search following user in an account
+[7] - Search not followers
+[8] - Unfollow not followers
+[9] - Unfollow'''
